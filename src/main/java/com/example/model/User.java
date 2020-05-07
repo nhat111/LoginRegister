133e2e23e4ef70a7369262e25d5bbf17599db992
Nguyễn Nhật <nhat10t3@gmail.com>
2020-05-07 10:16:33 +0700

    start Project

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,57 @@
+package com.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_name")
+	private String userName;
+	
+	@Column(name = "password")
+	private String password;
+
+	public User() {
+		
+	}
+
+	public User(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
