e68bca1ad57024114dec6f96dcd62e471a82ea19
Nguyễn Nhật <nhat10t3@gmail.com>
2020-05-08 14:29:33 +0700

    register Accout

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,9 +1,49 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.model.UserForm;
+import com.example.repository.UserRepository;
+import com.example.service.UserService;
 
 @Controller
+@RequestMapping("")
 public class UserController {
+	
+//	@Autowired
+	//private UserRepository userRepository;
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	@ModelAttribute("user")
+	public UserForm userForm() {
+		return new UserForm();
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+
+	@GetMapping("/registration")
+	public String showRegisterPage(Model model) {
+		return "register";
+	}
+	
+	@PostMapping("/registration")
+	public String registerAccout(@ModelAttribute("user") UserForm userForm) {
+		
+	userService.save(userForm);
+		
+		return "";
+	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/register.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Signup</title>
+</head>
+<body>
+	<form  method="post">
+		<table style="with: 50%">
+			<tr>
+				<td>UserName</td>
+				<td><input type="text" name="userName" /></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password" /></td>
+			</tr>
+		</table>
+		<input type="submit" value="Submit" />
+	</form>
+
+</body>
+</html>
